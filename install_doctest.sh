a52f9ade645ba5a1fcf8355d4f1a04e1bf80ff03
sbaldu <simone.balducci00@gmail.com>
2023-09-13 20:38:14 +0200

    Fix use of sudo in install_doctest script

--- install_doctest.sh
+++ install_doctest.sh
@@ -1,8 +1,13 @@
+#
+
 echo "Cloning the repository..."
 git clone https://github.com/doctest/doctest.git
+
 echo "Installing doctest..."
-mkdir -p /usr/local/include/doctest
-mv ./doctest/doctest/doctest.h /usr/local/include/doctest/doctest.h
+sudo mkdir -p /usr/local/include/doctest
+sudo mv ./doctest/doctest/doctest.h /usr/local/include/doctest/doctest.h
+
 echo "Cleaning up..."
 rm -rf ./doctest
+
 echo "Done!"
