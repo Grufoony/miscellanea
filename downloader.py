891e4edd349b14186affaa1b6466a3789daf3975
Grufoony <gregorio_2000@hotmail.it>
2025-01-29 22:54:16 +0100

    Updated

--- downloader.py
+++ downloader.py
@@ -1,57 +1,53 @@
-import pytube
-from pytube import YouTube
-from pytube import Playlist
 import os
-from moviepy.editor import AudioFileClip
 from tqdm import tqdm
+import yt_dlp
 
-if not os.path.exists("./temp"):
-    os.mkdir("./temp")
-if not os.path.exists("./mp3"):
-    os.mkdir("./mp3")
+YT_OPTIONS = {
+    'format': 'bestaudio[ext=m4a]',
+    'outtmpl': './m4a/%(title)s.%(ext)s',
+}
+if not os.path.exists("./m4a"):
+    os.mkdir("./m4a")
 
-print("Welcome to YouTube downloader v1.1 by Grufoony https://github.com/Grufoony")
-print("If you want to download a playlist just type 'playlist' or '.p' instead of the video url, next you can insert the url of the playlist.")
-print("If you want to exit just type '.q' instead of the url.")
+print("Welcome to YouTube downloader v2.0 by Grufoony https://github.com/Grufoony")
+print("If you want to download a playlist just type 'playlist' or '.p' instead of the video URL, next you can insert the URL of the playlist.")
+print("If you want to exit just type '.q' instead of the URL.")
 print("")
 
 pl = ["playlist", ".p"]
 leave = [".q"]
 
 while True:
-    url = input("Insert video url:\n")
+    url = input("Insert video URL:\n")
     if url in leave:
         break
     if url in pl:
-        url = input("Insert playlist url:\n")
+        url = input("Insert playlist URL:\n")
         if url in leave:
             break
-        playlist = Playlist(url)
-        print(f'Number of videos in playlist: {len(playlist.video_urls)}')
-        for video in tqdm(playlist.videos):
-            try:
-                audioStream = video.streams.get_by_itag("140")
-                audioStream.download(output_path="./temp")
-            except pytube.exceptions.AgeRestrictedError:
-                print("Age restricted video")
+        with yt_dlp.YoutubeDL({'format': 'bestaudio/best', 'outtmpl': './temp/%(id)s.%(ext)s', 'postprocessors': [{
+            'key': 'FFmpegAudio',
+            'preferredcodec': 'm4a',
+            'preferredquality': '192',
+        }]}) as ydl:
+            playlist_info = ydl.extract_info(url, download=False)
+            print(f'Number of videos in playlist: {len(playlist_info["entries"])}')
+            for entry in tqdm(playlist_info['entries']):
+                try:
+                    ydl.download([entry['url']])
+                except Exception as e:
+                    print(f"Error downloading {entry['title']}: {e}")
     else:
         try:
-            video = YouTube(url)
-            audioStream = video.streams.get_by_itag("140")
-            audioStream.download(output_path="./temp")
-        except pytube.exceptions.AgeRestrictedError:
-            print("Age restricted video")
+            with yt_dlp.YoutubeDL(YT_OPTIONS) as ydl:
+                ydl.download(url)
+        except Exception as e:
+            print(f"Error downloading video: {e}")
 
-# converting mp4 into mp3
+for file in os.listdir("./m4a"):
+    if file.endswith(".m4a"):
+        # remove official video tag
+        title = file.split("(")[0].strip()
+        os.rename(f"./m4a/{file}", f"./m4a/{title}.m4a")
 
-for file in tqdm(os.listdir('./temp')):
-    name = file[:-4]
-    audio = AudioFileClip(os.path.join("./temp", file))
-    audio.write_audiofile(os.path.join("./mp3", name + ".mp3"))
-    audio.close()
-
-# cleaning up temp folder
-
-filelist = [ f for f in os.listdir("./temp") if f.endswith(".mp4") ]
-for f in tqdm(filelist):
-    os.remove(os.path.join("./temp", f))
+print("Download and conversion complete!")
