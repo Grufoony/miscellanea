87317a3a7be6e94ce498e135ab2461f07b7d60ff
Grufoony <gregorio_2000@hotmail.it>
2025-07-10 09:55:41 +0200

    Update script

--- downloader.py
+++ downloader.py
@@ -1,17 +1,17 @@
-import os
-import yt_dlp
+from pathlib import Path
+import yt_dlp  # pip install yt-dlp
 
 __author__ = "Grufoony"
-__version__ = "2.1"
+__version__ = "2.2"
+
+out_dir = Path("./m4a")
+out_dir.mkdir(exist_ok=True)
 
 YT_OPTIONS = {
     "format": "bestaudio[ext=m4a]",
     "outtmpl": "./m4a/%(title)s.%(ext)s",
     "ignoreerrors": True,
 }
-if not os.path.exists("./m4a"):
-    os.mkdir("./m4a")
-
 print(
     f"Welcome to YouTube downloader v{__version__} by {__author__} https://github.com/Grufoony"
 )
@@ -27,15 +27,16 @@ while True:
     except Exception as e:
         print(f"Error downloading video: {e}")
 
-for file in os.listdir("./m4a"):
-    if file.endswith(".m4a"):
-        original_name = file
-        # remove extension
-        file = file[:-4]
-        # remove official video tag
-        title = file.split("[")[0].strip()
-        # Remove any string between parentheses with "official" in it
-        title = title.split("(Official")[0].strip()
-        os.rename(f"./m4a/{original_name}", f"./m4a/{title}.m4a")
+for file in out_dir.glob("*.m4a"):
+    title = file.stem
+    # remove official video tag
+    title = title.split("[")[0].strip()
+    # Remove any string between parentheses with "official" in it
+    title = title.split("(Official")[0].strip()
+
+    new_file = out_dir / f"{title}.m4a"
+    # Rename the file
+    if new_file != file:
+        file.rename(new_file)
 
 print("Download and conversion complete!")
